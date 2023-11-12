f5ee09e98f2d8ef0755dd21508785603a0c0d78a
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-11-12 09:14:41 -0700

    Added /user/[id] pages

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import makeStyles from "@mui/styles/makeStyles";
 import GridContainer from "/components/Grid/GridContainer.js";
 import GridItem from "/components/Grid/GridItem.js";
@@ -13,11 +13,11 @@ import Comments from "./Comments";
 const useStyles = makeStyles(sectionBlogInfoStyle);
 
 export default function Posts({ post }) {
-  
+
   const [comments, setComments] = useState([]);
   const [updateComment, setUpdateComment] = useState(false);
   const { _id, title, description, author, category, available } = post;
-  
+
   const [showComment, setShowComment] = React.useState(false);
 
   // //NEED HELP HERE
@@ -33,7 +33,7 @@ export default function Posts({ post }) {
   //   allComemnts = [];
   //   console.log("err: ",err);
   //   }    
-    
+
   //   console.log("allComemnts! ", allComemnts);
   //   setComments(allComemnts);
   //   setUpdateComment(false);
@@ -42,40 +42,40 @@ export default function Posts({ post }) {
 
   const updatePostStatus = () => {
     //NEED TO WORK! make a database call to update status of post
-    console.log("changing post status!")
+    console.log("changing post status!");
     setUpdateComment(true);
-  }
+  };
 
 
   const classes = useStyles();
   return (
     <>
-    <div className={classes.section}>
-      <GridContainer justifyContent="center">
-        <GridItem xs={12} sm={10} md={8}>
-          <Card plain profile className={classes.card}>
-            <GridContainer>
-              <GridItem xs={12} sm={4} md={4}>
-              <Image
-                src="/img/examples/bg2.jpg" 
-                alt="Item for recycling"
-                width={500}
-                height={500}
-              />                
-              </GridItem>
-              <GridItem xs={12} sm={8} md={8}>
-                <h4 className={classes.cardTitle}>{author.name}</h4>
-                <h3 className={classes.cardTitle}>{title}</h3>
-                <p className={classes.description}>
-                  {description}
-                </p>
-                <p className={classes.description}>
-                  {category}
-                </p>
-                <p className={classes.description}>
-                  {!available && "Not "} Available
-                </p>
-                {/* {comments && 
+      <div className={classes.section}>
+        <GridContainer justifyContent="center">
+          <GridItem xs={12} sm={10} md={8}>
+            <Card plain profile className={classes.card}>
+              <GridContainer>
+                <GridItem xs={12} sm={4} md={4}>
+                  <Image
+                    src="/img/examples/bg2.jpg"
+                    alt="Item for recycling"
+                    width={500}
+                    height={500}
+                  />
+                </GridItem>
+                <GridItem xs={12} sm={8} md={8}>
+                  <a href={"/user/" + author.id}><h4 className={classes.cardTitle}>{author.name}</h4></a>
+                  <h3 className={classes.cardTitle}>{title}</h3>
+                  <p className={classes.description}>
+                    {description}
+                  </p>
+                  <p className={classes.description}>
+                    {category}
+                  </p>
+                  <p className={classes.description}>
+                    {!available && "Not "} Available
+                  </p>
+                  {/* {comments && 
                 <Button 
                 color="primary" 
                 round className={classes.footerButtons}
@@ -84,23 +84,23 @@ export default function Posts({ post }) {
                       {showComment ? "Hide" : "Show"} Comments
                   </Button>}
                  */}
-                  {available && <Button 
-                color="primary" 
-                round className={classes.footerButtons}
-                onClick={updatePostStatus}
-                >
-                  Mark As Unavailable
-                  {/* Show "Mark As Unavailable" button only to the owner of the post  */}
-              </Button>}
-              </GridItem>
-            </GridContainer>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
+                  {available && <Button
+                    color="primary"
+                    round className={classes.footerButtons}
+                    onClick={updatePostStatus}
+                  >
+                    Mark As Unavailable
+                    {/* Show "Mark As Unavailable" button only to the owner of the post  */}
+                  </Button>}
+                </GridItem>
+              </GridContainer>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
 
       {/* {showComment && <Comments comments={comments} />} */}
-</>
+    </>
 
   );
 }
