e0432f55b8f9029ac85737cd4caca0c30818e4cd
MoMoZin <khinmomozin@gmail.com>
2023-11-12 08:39:01 -0700

    retrieve post by categroy

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { createPost } from '../../database/posts';
+import { createPost,getPosts } from '../../database/posts';
 import multer from 'multer';
 
 export const config = {
@@ -32,53 +32,66 @@ const upload = multer({
 });
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).end(); // Method Not Allowed
+  if (req.method === "GET") {
+    try {    
+      // console.log("category param: ", req.params.category);
+      const posts = await getPosts();
+      res.status(200).json(posts);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
-
-  try {
-    // Assuming 'image' is the field name
-    upload.single('image')(req, res, async (err) => {
-      if (err) {
-        console.error('Error uploading image:', err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-
-      const file = req.file;
-      console.log(req.file);
-      const { title, description } = req.body;
-      const uploadDir = path.join(process.cwd(), 'public/images'); // Update the path to your public folder
-
-      const postData = {
-        imageUrl: `/images/${file.originalname}`,
-        title: title,
-        description: description,
-        available: true,
-        category: "other",
-        author: {
-          id: 1,
-          name: "HARDCODED",
-          profileImage: "HARDCODED",
-          itemsDonated: 20,
-          itemsReceived: 12,
-          points: 200
+  else {
+    if (req.method !== 'POST') {
+      return res.status(405).end(); // Method Not Allowed
+    }
+  
+    try {
+      // Assuming 'image' is the field name
+      upload.single('image')(req, res, async (err) => {
+        if (err) {
+          console.error('Error uploading image:', err);
+          return res.status(500).json({ error: 'Internal Server Error' });
         }
-      };
-
-      // Create the 'public/images' directory if it doesn't exist
-      if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir, { recursive: true });
-      }
-
-      const filePath = path.join(uploadDir, file.originalname);
-      createPost(postData);
-      fs.writeFileSync(filePath, file.buffer);
-
-      res.status(201).json({ filePath: file.originalname });
-    });
-  } catch (error) {
-    console.error('Error uploading image:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  
+        const file = req.file;
+        console.log(req.file);
+        const { title, description } = req.body;
+        const uploadDir = path.join(process.cwd(), 'public/images'); // Update the path to your public folder
+  
+        const postData = {
+          imageUrl: `/images/${file.originalname}`,
+          title: title,
+          description: description,
+          available: true,
+          category: "other",
+          author: {
+            id: 1,
+            name: "HARDCODED",
+            profileImage: "HARDCODED",
+            itemsDonated: 20,
+            itemsReceived: 12,
+            points: 200
+          }
+        };
+  
+        // Create the 'public/images' directory if it doesn't exist
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir, { recursive: true });
+        }
+  
+        const filePath = path.join(uploadDir, file.originalname);
+        createPost(postData);
+        fs.writeFileSync(filePath, file.buffer);
+  
+        res.status(201).json({ filePath: file.originalname });
+      });
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
+  
 }
 
